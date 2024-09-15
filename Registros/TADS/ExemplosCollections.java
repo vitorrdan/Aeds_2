c39d43fa62cff6f0a72f54bf1063281bb86e971e
Daniel <danivitorcosta@gmail.com>
2024-09-15 17:58:16 -0300

    add collections.java

---
+++ Registros/TADS/ExemplosCollections.java
@@ -0,0 +1,42 @@
+package Registros.TADS;
+import java.util.*;
+
+
+
+public class ExemplosCollections {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+    
+        /*
+         * Exercicio 1
+         */
+        ArrayList<Integer> Notas = new ArrayList<>();
+        int soma = 0;
+
+        for(int i = 0; i < 5; i++){
+            int num = sc.nextInt();
+            Notas.add(num);
+            soma+=num;
+        }
+
+        float media = soma / 5;
+        System.out.println("Media: " + media);
+
+        for (Integer i : Notas) {
+            if(i > media){
+                System.out.println(i);
+            }
+            
+        }
+
+        sc.close();
+
+    
+
+    }
+
+
+}
+    
